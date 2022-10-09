5501621d198d5499f23ec48ed576975d1d3b20f9
nnmax <hi.max@foxmail.com>
2022-10-09 17:29:18 +0800

    style: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,8 @@ const eslintConfig = {
   env: {
     jest: true,
   },
-  extends: [
-    '@nnmax/eslint-config-typescript',
-    'plugin:storybook/recommended',
-    'prettier',
-  ],
-  ignorePatterns: [
-    'coverage/**',
-    'es/**',
-    'node_modules/**',
-    'storybook-static/**',
-    '.yarn/**',
-  ],
+  extends: ['@nnmax/eslint-config-typescript', 'plugin:storybook/recommended', 'prettier'],
+  ignorePatterns: ['coverage/**', 'es/**', 'node_modules/**', 'storybook-static/**', '.yarn/**'],
 }
 
 module.exports = eslintConfig
