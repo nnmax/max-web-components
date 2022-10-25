9aa199d43f5ee14743942fbc5bbdf9474705d9e7
nnmax <hi.max@foxmail.com>
2022-10-25 12:56:35 +0800

    feat: add Accordion component

---
+++ src/Accordion/Accordion.ts
@@ -0,0 +1,69 @@
+import styles from './styles/accordion.css' assert { type: 'css' }
+import AccordionPanel from './AccordionPanel'
+
+export default class Accordion extends HTMLElement {
+  static is = 'max-accordion'
+
+  #panels: AccordionPanel[]
+
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.adoptedStyleSheets.push(styles)
+    this.shadowRoot.innerHTML = `
+      <div part="container">
+        <slot></slot>
+      </div>
+    `
+  }
+
+  connectedCallback() {
+    const slot = this.shadowRoot.querySelector('slot')
+    slot.addEventListener('slotchange', this.#handleSlotChange.bind(this))
+    this.addEventListener('keydown', this.#handleKeyDown.bind(this))
+  }
+
+  #handleSlotChange(event: Event) {
+    const slot = event.target as HTMLSlotElement
+    this.#panels = this.#filterPanels(slot.assignedNodes())
+  }
+
+  #handleKeyDown(event: KeyboardEvent) {
+    const { key, metaKey, altKey } = event
+
+    if (metaKey || altKey || !this.#panels) return
+
+    const focusedIndex = this.#panels.findIndex((node) => node.hasAttribute('focused'))
+    const length = this.#panels.length
+    let index = -1
+
+    if (focusedIndex === -1) return
+    if (key === 'ArrowDown') {
+      index = (focusedIndex + 1) % length
+    }
+    if (key === 'ArrowUp') {
+      index = focusedIndex === 0 ? length - 1 : (focusedIndex - 1) % length
+    }
+    if (key === 'Home') {
+      index = 0
+    }
+    if (key === 'End') {
+      index = length - 1
+    }
+
+    if (index >= 0) {
+      this.#focusItem(index)
+    }
+  }
+
+  #focusItem(index: number) {
+    this.#panels[index].focusButton()
+  }
+
+  #filterPanels(nodes: Node[]): AccordionPanel[] {
+    if (Array.isArray(nodes)) {
+      return nodes.filter((node) => node instanceof AccordionPanel && !node.disabled) as AccordionPanel[]
+    }
+    return []
+  }
+}
